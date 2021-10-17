66042f02c2f60a24e99c53ecdf0a254fd07ef7ef
Nasreen Sayed <nasreen.store@gmail.com>
2021-10-18 02:20:52 +0400

    Passport

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 //var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
-var FileStore = require('session-file-store')(session)
-
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -43,6 +44,9 @@ app.use(session({
   store: new FileStore()
 
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -50,20 +54,13 @@ app.use('/users', usersRouter);
 function auth(req, res, next) {
   console.log(req.session);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
     err.status = 401;
     return next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err)
-    }
+    next();
   }
 
 }
